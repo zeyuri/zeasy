897a05f52660da8b005a3bf400991d764e45a96e
zeasy <jozeyuri@gmail.com>
2021-11-15 01:05:51 -0300

    feat(astro): move blog to use astro

---
+++ src/pages/blog/the-hardest-one.md
@@ -0,0 +1,11 @@
+---
+title: "The Hardest one"
+publishDate: "Monday, November 15 2021"
+layout: "../../layouts/BlogPost.astro"
+---
+
+A lot of things in my life were only exciting in the beginning... when the mood is still high, enthusiasm up there, _unless is about writing a blog_. Although my whole life I never felt without words, writing here is hard. Specially, in a place that is mine and mine alone. The words never came.
+
+It wasn't for a lack of trying. There were several attempts to find words and subjects to express here. I threw thoughts at life hoping to find what is most precious, _the meaning_.
+
+Unfortunately I've never had much success, I can't find much sense and reasons to write, but now I have no excuses, let's write even in the absence of meaning.
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,43 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./public/**/*.html', './src/**/*.{astro,js,jsx,ts,tsx,vue,svelte}'],
-  darkMode: 'media',
-  plugins: [require('@tailwindcss/typography')]
-};
+  mode: "jit",
+  purge: ["./public/**/*.html", "./src/**/*.{astro,js,jsx,ts,tsx,vue,svelte}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            color: "var(--tw-prose-body)",
+            code: {
+              color: "var(--tw-prose-code)",
+              backgroundColor: "var(--tw-prose-pre-bg)",
+            },
+            pre: { color: "var(--tw-prose-pre-code)" },
+            strong: {
+              color: "var(--tw-prose-headings)",
+            },
+            blockquote: {
+              color: "var(--tw-prose-quotes)",
+            },
+            h1: {
+              color: "var(--tw-prose-headings)",
+            },
+            h2: {
+              color: "var(--tw-prose-headings)",
+            },
+            h3: {
+              color: "var(--tw-prose-headings)",
+            },
+            h4: {
+              color: "var(--tw-prose-headings)",
+            },
+            h5: {
+              color: "var(--tw-prose-headings)",
+            },
+          },
+        },
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+}
